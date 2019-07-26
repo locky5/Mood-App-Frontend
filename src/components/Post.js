fd612ee3dbafadae03407462bd65b93f095a65fb
Aaron Smith <smitham50@gmail.com>
2019-07-26 13:09:54 -0400

    fixed conflicts

--- src/components/Post.js
+++ src/components/Post.js
@@ -27,7 +27,6 @@ class Post extends React.Component {
   render() {
     return (
       <div className="post" onClick={this.props.clickPost}>
-<<<<<<< HEAD
         <ul>
           {/* <li>
             <a href="#">
@@ -44,10 +43,6 @@ class Post extends React.Component {
             </a>
           </li>
           </ul>
-=======
-        <h1>{this.props.description}</h1>
-        {this.state.data ? this.getMoodName() : null}
->>>>>>> Kevin
       </div>
     )
   }

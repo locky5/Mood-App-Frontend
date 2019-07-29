58d7362ed0a3f115e0f781267a1530297f920d57
Aaron Smith <smitham50@gmail.com>
2019-07-29 12:57:00 -0400

    fixed login 500 error

--- src/containers/NavBar.js
+++ src/containers/NavBar.js
@@ -3,15 +3,18 @@ import {Link} from 'react-router-dom'
 
 class NavBar extends React.Component {
   render() {
+    console.log(this.props.currentUser)
     return (
       
     <div className="navbar">
+      
       <Link to="/posts">Home</Link>
       <Link to="/profile">Profile</Link>
       <Link to="/login">Log In</Link>
       <Link to="/signup">Signup</Link>
+      <Link to="#">{this.props.currentUser ? `Logout ${this.props.currentUser.name}` : null}</Link>
+     
     </div>
-      
     )
   }
 }

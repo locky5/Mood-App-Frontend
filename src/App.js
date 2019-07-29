f0c4106077b1be91b3bd7c312eedd93bf7922b6e
locky5 <daikevin19@yahoo.com>
2019-07-29 09:30:42 -0400

    did some stuff

--- src/App.js
+++ src/App.js
@@ -46,9 +46,7 @@ class App extends React.Component {
   }
 
 
-
   render() {
-    console.log(this.state)
     return (
       <div className="App">
           <NavBar currentUser={this.state.currentUser} />

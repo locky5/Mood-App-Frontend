17d08319376732aef1a1378fdf072a12d7020c9c
locky5 <daikevin19@yahoo.com>
2019-07-30 09:33:39 -0400

    fixing profile

--- src/containers/Graph.js
+++ src/containers/Graph.js
@@ -1,38 +1,52 @@
 import React from 'react'
 import ThisLineChart from '../components/ThisLineChart'
 import ThisPieChart from '../components/ThisPieChart'
+import {Link} from 'react-router-dom'
 
 export default class Graph extends React.Component {
 
   render() {
     return (
       <div>
-        <br></br>
-        <h1>Hi, {this.props.currentUser.name}</h1>
-        <br></br>
+        {
+          this.props.currentUser ?
+          <div>
+            <br></br>
+            <h1>Hi, {this.props.currentUser.name}</h1>
+            <br></br>
 
-        <h1>Here's Some Of Your Popular Posts: </h1>
-        {this.props.currentUser.posts.sort((a, b) =>
-        (a.likes < b.likes) ? 1: -1).slice(0,4).map(post =>
-          <div className="post">
-            <div className="post-it">
-              {post.description}
-              <br></br>
-              Likes: {post.likes}
-            </div>
+            <h1>Here's Some Of Your Popular Posts: </h1>
+            {this.props.currentUser.posts.sort((a, b) =>
+            (a.likes < b.likes) ? 1: -1).slice(0,4).map(post =>
+              <div className="post">
+                <div className="post-it">
+                  {post.description}
+                  <br></br>
+                  Likes: {post.likes}
+                </div>
+              </div>
+            )}
+
+            <br></br>
+            <h2>Here's Some Analytics Of Your Moods:</h2>
+            <ThisLineChart
+              posts={this.props.currentUser.posts}
+              moods={this.props.moods}
+            />
+            <ThisPieChart
+              posts={this.props.currentUser.posts}
+              moods={this.props.moods}
+            />
           </div>
-        )}
-
-        <br></br>
-        <h2>Here's Some Analytics Of Your Moods:</h2>
-        <ThisLineChart
-          posts={this.props.currentUser.posts}
-          moods={this.props.moods}
-        />
-        <ThisPieChart
-          posts={this.props.currentUser.posts}
-          moods={this.props.moods}
-        />
+          :
+          <div>
+            <br></br>
+            <br></br>
+            <p>Please sign in!</p>
+            <Link to='/login'>Login
+            </Link>
+          </div>
+        }
       </div>
     )
   }
